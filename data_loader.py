6a3cb26eb81ae82c1bec669ba063729da83087dd
史丹利复合田 <tsycnh@126.com>
2018-03-25 10:27:02 +0800

    ...

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import scipy
 import scipy.misc
 from glob import glob
 import numpy as np
+import random
 
 class DataLoader():
     def __init__(self, dataset_name, img_res=(128, 128)):
@@ -12,17 +13,18 @@ class DataLoader():
     def load_data(self, domain, batch_size=1, is_testing=False):
         data_type = "train%s" % domain if not is_testing else "test%s" % domain
         path = glob('./datasets/%s/%s/*' % (self.dataset_name, data_type))
+        random.shuffle(path)
 
-        batch_images = np.random.choice(path, size=batch_size)
-
+        # batch_images = np.random.choice(path, size=batch_size)
+        batch_images = path[0:batch_size]
         imgs = []
         for img_path in batch_images:
             img = self.imread(img_path)
             if not is_testing:
                 img = scipy.misc.imresize(img, self.img_res)
 
-                if np.random.random() > 0.5:
-                    img = np.fliplr(img)
+                # if np.random.random() > 0.5:
+                #     img = np.fliplr(img)
             else:
                 img = scipy.misc.imresize(img, self.img_res)
             imgs.append(img)
@@ -44,3 +46,11 @@ class DataLoader():
 
     def get_imgs_path(self):
         return self.img_paths
+
+    def get_imgs_name(self):
+        img_names =self.img_paths
+        for i,path in enumerate(img_names):
+            print(path)
+            b = path.split('/')
+            img_names[i] = b[-1]
+        return img_names
--- just_for_translation.py
+++ just_for_translation.py
@@ -2,4 +2,4 @@ from cyclegan_plates import CycleGAN
 
 gan = CycleGAN()
 
-gan.translate('good_model/model_epoch_1800.h5')
+gan.translate('good_model/model_epoch_1800.h5',total_num=1500)
