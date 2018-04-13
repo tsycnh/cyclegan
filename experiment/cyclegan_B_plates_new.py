5e0f8fdd41c29de2f23d305a9aea9e785cd78056
史丹利复合田 <tsycnh@126.com>
2018-04-13 10:41:22 +0800

    ..

--- experiment/cyclegan_B_plates_new.py
+++ experiment/cyclegan_B_plates_new.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     cls = ['Cr','In','Pa','PS','RS','Sc']
     # 开始训练
     for kind in cls:#_B_plates_new
-        gan = CycleGAN('/plates/单独训练集_B_plates_new/'+kind, appendix=kind+'_new_plates',lamb=[1,5,1])#lamb:gan,cycle,id
+        gan = CycleGAN('/plates/单独训练集_B_plates_new/'+kind, appendix=kind+'_new_plates_151',lamb=[1,5,1])#lamb:gan,cycle,id
         gan.train(epochs=2001, batch_size=2, save_interval=100)
 
     for kind in cls:#_B_plates_new
-        gan = CycleGAN('/plates/单独训练集_B_aluminum/'+kind, appendix=kind+'_new_plates',lamb=[1,5,1])#lamb:gan,cycle,id
+        gan = CycleGAN('/plates/单独训练集_B_aluminum/'+kind, appendix=kind+'_new_plates_151',lamb=[1,5,1])#lamb:gan,cycle,id
         gan.train(epochs=2001, batch_size=2, save_interval=100)
