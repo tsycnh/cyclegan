bf5e5ccbdd6b3ec041ab65bcc0b34374b442f19c
史丹利复合田 <tsycnh@126.com>
2018-04-13 10:40:37 +0800

    增加铝板实验

--- experiment/cyclegan_B_plates_new.py
+++ experiment/cyclegan_B_plates_new.py
@@ -2,10 +2,14 @@ from cyclegan_plates import CycleGAN
 import os
 if __name__ == "__main__":
     # 控制显卡可见性
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
     cls = ['Cr','In','Pa','PS','RS','Sc']
     # 开始训练
     for kind in cls:#_B_plates_new
-        gan = CycleGAN('/plates/单独训练集/'+kind, appendix=kind+'_new_plates',lamb=[1,5,1])#lamb:gan,cycle,id
+        gan = CycleGAN('/plates/单独训练集_B_plates_new/'+kind, appendix=kind+'_new_plates',lamb=[1,5,1])#lamb:gan,cycle,id
+        gan.train(epochs=2001, batch_size=2, save_interval=100)
+
+    for kind in cls:#_B_plates_new
+        gan = CycleGAN('/plates/单独训练集_B_aluminum/'+kind, appendix=kind+'_new_plates',lamb=[1,5,1])#lamb:gan,cycle,id
         gan.train(epochs=2001, batch_size=2, save_interval=100)
