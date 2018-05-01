7bd557a454c7fb05cb37d3788ffcd1d64b88ccee
tsycnh <tsycnh@126.com>
2018-05-01 16:49:46 +0800

    opps

--- experiment/cyclegan_B_plates_new.py
+++ experiment/cyclegan_B_plates_new.py
@@ -6,10 +6,15 @@ if __name__ == "__main__":
 
     cls = ['Cr','In','Pa','PS','RS','Sc']
     # 开始训练
+    lambda_gan = 1
+    lambda_cycle = 10
+    lambda_id = 0.5
     for kind in cls:#_B_plates_new
-        gan = CycleGAN('/plates/单独训练集_B_plates_new/'+kind, appendix=kind+'_new_plates_151',lamb=[1,5,1])#lamb:gan,cycle,id
+        gan = CycleGAN('/plates/单独训练集_B_plates_new/'+kind, appendix=kind+'_new_plates_%d%d%d'%(lambda_gan,lambda_cycle,lambda_id),
+                       lamb=[lambda_gan,lambda_cycle,lambda_id])#lamb:gan,cycle,id
         gan.train(epochs=2001, batch_size=2, save_interval=100)
 
     for kind in cls:#_B_plates_new
-        gan = CycleGAN('/plates/单独训练集_B_aluminum/'+kind, appendix=kind+'_new_plates_151',lamb=[1,5,1])#lamb:gan,cycle,id
+        gan = CycleGAN('/plates/单独训练集_B_aluminum/'+kind, appendix=kind+'_aluminum_%d%d%d'%(lambda_gan,lambda_cycle,lambda_id),
+                       lamb=[lambda_gan,lambda_cycle,lambda_id])#lamb:gan,cycle,id
         gan.train(epochs=2001, batch_size=2, save_interval=100)
